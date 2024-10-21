e5a8413a638df97b23e7ea2c7ae8fbed3d101556
Diggo <rdiggosilva@gmail.com>
2024-10-21 14:08:14 -0300

    Separated FeedCell's file

--- MyMusic/App/Scenes/Feed/FeedViewController.swift
+++ MyMusic/App/Scenes/Feed/FeedViewController.swift
@@ -50,46 +50,6 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
-class FeedCell: UITableViewCell {
-    static let identifier: String = "FeedCell"
-    
-    lazy var clientName: UILabel = {
-        let lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = .preferredFont(forTextStyle: .subheadline)
-        return lbl
-    }()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupView()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupView() {
-        setHierarchy()
-        setConstraints()
-    }
-    
-    private func setHierarchy () {
-        backgroundColor = .systemBackground
-        addSubview(clientName)
-    }
-    
-    private func setConstraints() {
-        NSLayoutConstraint.activate([
-            clientName.centerYAnchor.constraint(equalTo: centerYAnchor),
-            clientName.topAnchor.constraint(equalTo: topAnchor, constant: 20),
-            clientName.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
-            clientName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            clientName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-        ])
-    }
-}
-
 class Client {
     let name: String
     
