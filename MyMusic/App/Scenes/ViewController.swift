9024e5f5bddb1800965e2827d7f290f30b45d024
Diggo <rdiggosilva@gmail.com>
2024-10-20 22:01:05 -0300

    Starting to build the app

---
+++ MyMusic/App/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Diggo Silva on 20/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
