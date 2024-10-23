dece99ca6921906f36e02b714e6018eeee57d181
Diggo <rdiggosilva@gmail.com>
2024-10-23 10:59:20 -0300

    Created GameCell file and implemented a CustomCell

--- MyMusic/App/Scenes/Feed/FeedViewController.swift
+++ MyMusic/App/Scenes/Feed/FeedViewController.swift
@@ -84,6 +84,13 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
 
 class GameViewController: UIViewController {
     
+    let gameView = GameView()
+    
+    override func loadView() {
+        super.loadView()
+        view = gameView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavBar()
@@ -96,11 +103,26 @@ class GameViewController: UIViewController {
     }
     
     private func setDelegatesAndDataSources() {
-        
+        gameView.tableView.delegate = self
+        gameView.tableView.dataSource = self
     }
     
     @objc func addGameAlert() {
-        
+        print("addGameAlert")
+    }
+}
+
+extension GameViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GameCell.identifier, for: indexPath) as! GameCell
+//        cell.textLabel?.text = "Game \(indexPath.row)"
+        cell.gameName.text = "Game \(indexPath.row)"
+        cell.gamePrice.text = "R$ 100,00"
+        return cell
     }
 }
 
@@ -108,7 +130,7 @@ class GameView: UIView {
     lazy var tableView: UITableView = {
         let tv = UITableView()
         tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tv.register(GameCell.self, forCellReuseIdentifier: GameCell.identifier)
         return tv
     }()
     
@@ -137,6 +159,65 @@ class GameView: UIView {
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
+
+class GameCell: UITableViewCell {
+    static let identifier: String = "GameCell"
+    
+    lazy var gameName: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = .preferredFont(forTextStyle: .headline)
+        lbl.text = "Game Name"
+        return lbl
+    }()
+    
+    lazy var gamePrice: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = .preferredFont(forTextStyle: .subheadline)
+        lbl.text = "R$ 4500,00"
+        return lbl
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(game: Game) {
+        gameName.text = "Game Name"
+        gamePrice.text = "R$ 4500,00"
+        self.accessoryType = .disclosureIndicator
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy () {
+        backgroundColor = .systemBackground
+        addSubview(gameName)
+        addSubview(gamePrice)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            gameName.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            gameName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            gameName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            gamePrice.topAnchor.constraint(equalTo: gameName.bottomAnchor, constant: 10),
+            gamePrice.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            gamePrice.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            gamePrice.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
         ])
     }
 }
