738e9716d24d5ec8ea96d722dbcfc750b312af4f
Diggo <rdiggosilva@gmail.com>
2024-10-23 10:15:09 -0300

    Created Game object

--- MyMusic/App/Model/Client.swift
+++ MyMusic/App/Model/Client.swift
@@ -9,8 +9,22 @@ import Foundation
 
 class Client: Codable {
     let name: String
+    let games: [Game]
     
-    init(name: String) {
+    init(name: String, games: [Game] = []) {
         self.name = name
+        self.games = games
+    }
+}
+
+class Game: Codable {
+    let name: String
+//    var price: Double {
+//
+//    }
+    
+    init(name: String/*, price: Double*/) {
+        self.name = name
+//        self.price = price
     }
 }
