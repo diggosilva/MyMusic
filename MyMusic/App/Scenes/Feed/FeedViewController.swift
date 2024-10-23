6b245b912042a2b0ec79720aede9db1d5cbe4e11
Diggo <rdiggosilva@gmail.com>
2024-10-23 10:38:00 -0300

    Created GameViewController and GameView files, implemented tableView in Gameview

--- MyMusic/App/Scenes/Feed/FeedViewController.swift
+++ MyMusic/App/Scenes/Feed/FeedViewController.swift
@@ -86,7 +86,57 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavBar()
+        setDelegatesAndDataSources()
+    }
+    
+    private func setNavBar() {
         title = "Jogos"
-        view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector (addGameAlert))
+    }
+    
+    private func setDelegatesAndDataSources() {
+        
+    }
+    
+    @objc func addGameAlert() {
+        
+    }
+}
+
+class GameView: UIView {
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy () {
+        backgroundColor = .systemBackground
+        addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
     }
 }
