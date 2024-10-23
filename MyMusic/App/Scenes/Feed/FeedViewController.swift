7b654d201a963f45f04190a9865de00964f2fb0d
Diggo <rdiggosilva@gmail.com>
2024-10-23 11:30:32 -0300

    Created GameViewModel

--- MyMusic/App/Scenes/Feed/FeedViewController.swift
+++ MyMusic/App/Scenes/Feed/FeedViewController.swift
@@ -221,3 +221,28 @@ class GameCell: UITableViewCell {
         ])
     }
 }
+
+protocol GameViewModelProtocol {
+    func numberOfRowsInSection() -> Int
+    func cellForRowAt(indexPath: IndexPath) -> Game
+}
+
+class GameViewModel: GameViewModelProtocol {
+    let userDefaultsKey = "userDefaultsKey"
+    let userDefaults = UserDefaults.standard
+    var listGame: [Game] = []
+    let client: Client
+    
+    init(client: Client) {
+        self.client = client
+//        self.listGame = client.games  // Sugestao xcode
+    }
+    
+    func numberOfRowsInSection() -> Int {
+        listGame.count
+    }
+    
+    func cellForRowAt(indexPath: IndexPath) -> Game {
+        listGame[indexPath.row]
+    }
+}
