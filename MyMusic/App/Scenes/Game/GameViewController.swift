c5b8eef6e9de071d690d045726e20151e6e59cc1
Diggo <rdiggosilva@gmail.com>
2024-10-28 15:29:14 -0300

    Created class SongView and created  tableView

--- MyMusic/App/Scenes/Game/GameViewController.swift
+++ MyMusic/App/Scenes/Game/GameViewController.swift
@@ -76,3 +76,40 @@ extension GameViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
 }
+
+class SongView: UIView {
+    lazy var tableview: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy () {
+        backgroundColor = .systemBackground
+        addSubview(tableview)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableview.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableview.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableview.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
