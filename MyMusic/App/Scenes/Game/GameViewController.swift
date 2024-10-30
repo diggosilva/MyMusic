0b99e3708574c8764c90144499d864e1d4ecc7fa
Diggo <rdiggosilva@gmail.com>
2024-10-30 10:35:58 -0300

    Separate SongViewModel's file

--- MyMusic/App/Scenes/Game/GameViewController.swift
+++ MyMusic/App/Scenes/Game/GameViewController.swift
@@ -84,32 +84,3 @@ extension GameViewController: UITableViewDelegate, UITableViewDataSource {
         navigationController?.pushViewController(songVC, animated: true)
     }
 }
-
-protocol SongViewModelProtocol {
-    func numberOfRowsInSection()  -> Int
-    func cellForRowAt(indexPath: IndexPath) -> Song
-    func addSong(songTitle: String, songPrice: String)
-}
-
-class SongViewModel: SongViewModelProtocol {
-    let game: Game
-    let repository = Repository()
-    
-    init(game: Game) {
-        self.game = game
-    }
-    
-    func numberOfRowsInSection() -> Int {
-        return game.songs.count
-    }
-    
-    func cellForRowAt(indexPath: IndexPath) -> Song {
-        return game.songs[indexPath.row]
-    }
-    
-    func addSong(songTitle: String, songPrice: String) {
-        let song = Song(title: songTitle, price: Double(songPrice) ?? 0.0)
-        game.songs.append(song)
-//        repository.updateUser(client: <#T##Client#>)
-    }
-}
---
+++ MyMusic/App/Scenes/Song/SongViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  SongViewModelProtocol.swift
+//  MyMusic
+//
+//  Created by Diggo Silva on 30/10/24.
+//
+
+import Foundation
+
+protocol SongViewModelProtocol {
+    func numberOfRowsInSection()  -> Int
+    func cellForRowAt(indexPath: IndexPath) -> Song
+    func addSong(songTitle: String, songPrice: String)
+}
+
+class SongViewModel: SongViewModelProtocol {
+    let game: Game
+    let repository = Repository()
+    
+    init(game: Game) {
+        self.game = game
+    }
+    
+    func numberOfRowsInSection() -> Int {
+        return game.songs.count
+    }
+    
+    func cellForRowAt(indexPath: IndexPath) -> Song {
+        return game.songs[indexPath.row]
+    }
+    
+    func addSong(songTitle: String, songPrice: String) {
+        let song = Song(title: songTitle, price: Double(songPrice) ?? 0.0)
+        game.songs.append(song)
+//        repository.updateUser(client: <#T##Client#>)
+    }
+}
