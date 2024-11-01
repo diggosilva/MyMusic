4a599c6603c73d86fef939c8564e57adf1050c04
Diggo <rdiggosilva@gmail.com>
2024-11-01 11:49:33 -0300

    Each added client receives a unique ID so there is no problem with duplicate clients.

--- MyMusic/App/Model/Client.swift
+++ MyMusic/App/Model/Client.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 class Client: Codable {
+    let id: UUID
     let name: String
     var games: [Game]
     
     init(name: String, games: [Game] = []) {
+        self.id = UUID()
         self.name = name
         self.games = games
     }
